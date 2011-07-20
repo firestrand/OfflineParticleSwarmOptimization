061b0c5d7e2f8ea4c36f0bf839725aa1127f70d2
Travis Silvers <firestrand@gmail.com>
2011-07-19 23:59:59 -0400

    Initial Project Commit

---
+++ OfflineParticleSwarmOptimization/VariableParticleSwarmOptimizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace OfflineParticleSwarmOptimization
+{
+    public class VariableParticleSwarmOptimizer
+    {
+        private readonly int _dimensions;
+        public VariableParticleSwarmOptimizer(int dimensions)
+        {
+            if(dimensions <= 0) throw new ArgumentOutOfRangeException("dimensions");
+            _dimensions = dimensions;
+        }
+        public double[] ReportFitness(double fitness)
+        {
+            var result = new double[_dimensions];
+            return result;
+        }
+    }
+}
---
+++ OfflineParticleSwarmOptimizationTest/VariableParticleSwarmOptimizerTest.cs
@@ -0,0 +1,31 @@
+﻿using OfflineParticleSwarmOptimization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OfflineParticleSwarmOptimizationTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for VariableParticleSwarmOptimizerTest and is intended
+    ///to contain all VariableParticleSwarmOptimizerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class VariableParticleSwarmOptimizerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DimensionOfZeroThrowsAnException()
+        {
+            int dimensions = 0;
+            var target = new VariableParticleSwarmOptimizer(dimensions);
+        }
+        [TestMethod]
+        public void ReportFitnessReturnsArraySizeEqualToDimensions()
+        {
+            int dimensions = 1;
+            var target = new VariableParticleSwarmOptimizer(dimensions);
+            Assert.AreEqual(dimensions,target.ReportFitness(0.0).Length);
+        }
+    }
+}
